f8721eac1a67e20267ba289ac9d9f5a08a3b4cba
Mahmoud Sameh <Mahmoudsameh25@gmail.com>
2023-09-09 16:28:36 +0300

    Flash Chat
    Initial Commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Sameh on 31/05/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct K{
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    static let appName="⚡️FlashChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
+
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    var welcomeTitle=K.appName
+    var charIndex=0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.scrollEdgeAppearance=navigationController?.navigationBar.standardAppearance
+        
+        navigationController?.navigationBar.isHidden=true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden=false
+    }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        
+       
+        
+        titleLabel.text=""
+        for char in welcomeTitle{
+            Timer.scheduledTimer(withTimeInterval: 0.1*Double(charIndex), repeats: false){ (timer) in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex+=1
+        }
        
     }
     
